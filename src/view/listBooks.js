70d3a29caf305153a5bdfcc7841143cb7672f9bd
Akshay Khot <akshay.7277@gmail.com>
2016-02-10 16:14:24 -0800

    Refactor RETRIEVE use case

--- src/view/listBooks.js
+++ src/view/listBooks.js
@@ -4,20 +4,19 @@
 library.view.listBooks = {
     setupUI: function() {
         var tableBodyE1  = document.querySelector("table#books>tbody");
-        var keys = [], key="";
+        var keys = [], key="", row={};
         //load all book objects
-        book.loadAll();
-        keys = Object.keys(book.instances);
+        Book.loadAll();
+        keys = Object.keys(Book.instances);
 
         //for each book, create a table row with cells for the three attributes
         //-1 makes sure the new elements are appended at the end
         for(var i=0; i<keys.length; i++) {
             key = keys[i];
             row = tableBodyE1.insertRow();
-            row.insertCell(-1).textContent = book.instances[key].isbn;
-            row.insertCell(-1).textContent = book.instances[key].title;
-            row.insertCell(-1).textContent = book.instances[key].year;
+            row.insertCell(-1).textContent = Book.instances[key].isbn;
+            row.insertCell(-1).textContent = Book.instances[key].title;
+            row.insertCell(-1).textContent = Book.instances[key].year;
         }
-
     }
-}
+};
